ef7272e118e36e6c8418e1f240e09d00ec62cd3c
Daniel <danielksklyar@gmail.com>
2020-07-14 17:52:27 -0700

    -Added react-hook eslint plugin to help with dependancy array bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
 		},
 	},
 	extends: [
+		"plugin:react-hooks/recommended", // helpers for react-hooks
 		"plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
 		"plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
 		"prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
